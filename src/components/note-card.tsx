89fa196b707b6d5ae00dbe1c244bc5ef5d36a1ce
Calvin <calvinsoares17@gmail.com>
2024-02-09 17:35:08 -0300

    feat: componente de card notas

---
+++ src/components/note-card.tsx
@@ -0,0 +1,42 @@
+import * as Dialog from "@radix-ui/react-dialog" 
+import { formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
+
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
+    return (
+      <Dialog.Root>
+        <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-600 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-purple-400' >
+        <span className='text-sm font-medium text-slate-200'>
+            {note.date.toISOString()}
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+            {note.content}
+          </p>
+          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/70 to-black/0 pointer-events-none'/>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="inset-0 fixed bg-black/60" />
+          <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none ">
+                <div className="flex flex-1 flex-col gap-3 p-5">
+                  <span className='text-sm font-medium text-slate-200'>
+                   {formatDistanceToNow(note.date,  { locale: ptBR, addSuffix: true })}
+                  </span>
+
+                  <p className='text-sm leading-6 text-slate-400'>
+                    {note.content}
+                  </p>
+                </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+        
+      </Dialog.Root>
+    )
+}
